b89d07cb6a20f525001681b09a2ce92c3d4a8f87
ToastBubbles <jeffneal11@gmail.com>
2024-02-06 12:57:21 -0600

    added collection search

---
+++ CollectionTools.js
@@ -0,0 +1,163 @@
+try {
+    console.log("working");
+
+    function increaseItemsPerPage() {
+        let dropdown = document.querySelectorAll("#MPI-viewPerPage-dropdown")[1];
+        if (!dropdown) console.log("Element with ID 'MPI-viewPerPage-dropdown' not found");
+        let increasedView = document.createElement('option');
+        increasedView.textContent = "10000 per page";
+        increasedView.setAttribute("value", '10000');
+        dropdown.appendChild(increasedView);
+        increasedView.selected = true;
+    }
+    increaseItemsPerPage()
+
+    let body = document.getElementById("bl-my-personal-inventory");
+    if (!body) console.log("Element with ID 'bl-my-personal-inventory' not found");
+    let container = body.querySelector(".personal-inventory__container");
+    if (!container) console.log("Element with class 'personal-inventory__container' not found");
+    let childSections = container.getElementsByTagName("section");
+
+    let allItems = []
+    let items = []
+
+
+
+    let inventoryContainer
+    function reload() {
+        allItems = []
+        items = []
+        console.log("reloading");
+        fetchInventoryData().then(() => {
+            inventoryContainer = childSections[childSections.length - 1].lastElementChild
+            // console.log(inventoryContainer);
+
+            for (let i = 0; i < inventoryContainer.children.length; i++) {
+                let infoNode = inventoryContainer.children[i].children[2]
+                // let partNumber = infoNode.firstChild.firstChild.textContent
+                let partName = infoNode.firstChild.textContent.split(': ').join('');
+                let color = infoNode.lastChild.textContent
+                allItems.push({
+                    id: i,
+                    element: inventoryContainer.children[i],
+                    content: `${partName} ${color}`
+                })
+
+
+            }
+
+
+        });
+    }
+    reload()
+
+    let newSearchConatiner = generateSearchbar()
+
+    container.insertBefore(newSearchConatiner, childSections[childSections.length - 1])
+
+    function fetchInventoryData() {
+        // Simulate an API call with a setTimeout
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // Resolve the promise after the API call completes
+                // ready = true
+
+                let sb = document.getElementById("my-collection-searchbar")
+                if (sb) sb.disabled = false
+
+                resolve();
+            }, 1500); // Adjust the delay as needed
+        });
+    }
+
+    // let parentElement = document.getElementById('parentElement');
+
+    // Attach an event listener to the parent element
+    container.addEventListener('input', function (event) {
+        // Check if the event occurred on the search bar
+        if (event.target.id === 'my-collection-searchbar') {
+            // Handle search functionality
+            let searchTerm = event.target.value.toLowerCase();
+
+            let filteredItems = allItems.filter(item => {
+                return item.content.toLowerCase().includes(searchTerm);
+            });
+            updateUI(filteredItems);
+        }
+    });
+
+    function generateSearchbar() {
+        let section = document.createElement('section');
+        section.classList.add("personal-inventory__add-item", "personal-inventory__container-full-width")
+        let div0 = document.createElement('div')
+        div0.classList.add("personal-inventory__add-item-title")
+        div0.style.paddingBottom = '0.5em'
+        div0.textContent = "Search within your collection"
+        let div1 = document.createElement('div');
+        div1.style.display = 'flex'
+        let searchbar = document.createElement('input');
+        searchbar.classList.add("text-input", "input-button-set__input", "l-full-width")
+        searchbar.setAttribute('placeholder', 'Search for parts in your Collection');
+        searchbar.setAttribute('id', 'my-collection-searchbar');
+
+        // searchbar.addEventListener('input', function (event) {
+        //     let searchTerm = event.target.value.toLowerCase(); // Get the search term and convert it to lowercase
+        //     console.log(searchTerm);
+
+        //     // Filter items based on the search term
+        //     let filteredItems = items.filter(item => {
+        //         // Check if the item's content string contains the search term
+        //         return item.content.toLowerCase().includes(searchTerm);
+        //     });
+
+        //     // Update the UI to display only the filtered items
+        //     updateUI(filteredItems);
+        // });
+
+
+        searchbar.disabled = true
+        let button = document.createElement('button');
+        button.classList.add("personal-inventory__input-search-magnifier-button", "magnifire-0")
+        let i = document.createElement('i');
+        i.classList.add('far', 'fa-search');
+
+        button.appendChild(i);
+        div1.appendChild(searchbar);
+        div1.appendChild(button);
+        section.appendChild(div0)
+        section.appendChild(div1);
+        return section;
+    }
+    function updateUI(filteredItems) {
+        // Clear the current UI
+        inventoryContainer.innerHTML = '';
+        console.log(filteredItems);
+
+        // Rebuild the UI with filtered items
+        filteredItems.forEach(item => {
+            inventoryContainer.appendChild(item.element);
+        });
+    }
+
+    function addEventListenerToTabs() {
+        let tabSets = document.getElementById("MPI-nav-mySets"),
+            tabParts = document.getElementById("MPI-nav-myParts"),
+            tabMinifigs = document.getElementById("MPI-nav-myMinifigures"),
+            tabBooks = document.getElementById("MPI-nav-myBooks"),
+            tabGear = document.getElementById("MPI-nav-myGears"),
+            tabCatalogs = document.getElementById("MPI-nav-myCatalogs");
+
+        let tabs = [tabSets, tabParts, tabMinifigs, tabBooks, tabGear, tabCatalogs]
+
+        tabs.forEach(tab => {
+            tab.addEventListener('click', function (event) {
+                reload()
+            });
+        })
+
+    }
+    addEventListenerToTabs()
+
+} catch (error) {
+    console.error("Error:", error.message);
+}
